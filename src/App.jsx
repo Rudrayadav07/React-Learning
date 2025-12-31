f1e16d57d65e334b73380fb65daafeab55069141
Rudrayadav007 <rudrayadav87701@gmail.com>
2025-12-31 06:33:20 +0530

    UseStateHook

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,48 @@
-import React from "react";
+// import React from "react";
 
+// const App = () => {
+//   const profile = [
+//     { name: "Rudra", age: 21, city: "Indore" },
+//     { name: "naman", age: 20, city: "Indore" },
+//     { name: "ashu", age: 25, city: "Indore" },
+//   ];
+//  const UpdatedData = profile.map((profile)=>{
+//   return<li>
+//      <span>{profile.name}</span> ||
+//      <small>{ profile.age}</small> || 
+//     <span>{profile.city}</span>
+//     </li>;
+//  });
+
+//   return (
+//     <div>
+//       <h1>Rendring json</h1>
+//      <ol>
+//       {UpdatedData}
+//      </ol>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+// learning usestate hook 
+import { useState } from 'react'
 const App = () => {
-  const profile = [
-    { name: "Rudra", age: 21, city: "Indore" },
-    { name: "naman", age: 20, city: "Indore" },
-    { name: "ashu", age: 25, city: "Indore" },
-  ];
- const UpdatedData = profile.map((profile)=>{
-  return<li>
-     <span>{profile.name}</span> ||
-     <small>{ profile.age}</small> || 
-    <span>{profile.city}</span>
-    </li>;
- });
+  const[UserName,SetUserName]= useState("Rudra")
 
+  const ChangeUserName = () =>{
+    SetUserName("Naman")
+  };
   return (
     <div>
-      <h1>Rendring json</h1>
-     <ol>
-      {UpdatedData}
-     </ol>
+      <h1>Learning useState Hook</h1>
+       <h2> userName:{UserName}
+       </h2>
+       <button onClick={ChangeUserName}>Click</button>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
